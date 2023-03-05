ec385e915d32ff5149e09a32ca351b58212b181d
coortiz1 <105026533+coortiz1@users.noreply.github.com>
2023-03-05 00:48:44 -0800

    save across reload

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-let click = 0;
-function buttonClick(){
-    click = click + 1;
-    document.getElementById('num').innerHTML = click;
-}
-
 function expressions(){
     var image = document.getElementById('myImage');
     if (image.src.match("https://i.imgur.com/q04YrKj")){
@@ -23,6 +17,7 @@ function expressions(){
     }                
 }
 
+
 function cross(id){
     var elem = document.getElementById(id);
     if (elem.style.textDecoration === "line-through red"){
@@ -30,16 +25,64 @@ function cross(id){
     }
     else{
         elem.style.textDecoration = "line-through red";
-
     }
 
 }
+
+let click = 0;
+if (localStorage.getItem("exerciseAmount") === null){
+    localStorage.setItem("exerciseAmount","0");
+}
+document.getElementById('cross1').innerHTML = 'Exercise (' + localStorage.getItem("exerciseAmount") + ' times)';
+function exercise(){
+    click = parseInt(localStorage.getItem("exerciseAmount"));
+
+    if (document.getElementById('cross1').style.textDecoration === "line-through red"){
+        click = click + 1;
+        document.getElementById('cross1').innerHTML = 'Exercise (' + click.toString() + ' times)';        
+    }
+    else if (document.getElementById('cross1').style.textDecoration === "none" && click === 5){
+        click = 0
+        document.getElementById('cross1').innerHTML = 'Exercise (0 times)'
+    }
+    localStorage.setItem("exerciseAmount",click.toString());
+}
 const ids = ['cross1','cross2','cross3','cross4','cross5','cross6'];
+
+
 function reset(){
     for (let i = 0; i < ids.length; i++){
         if (document.getElementById(ids[i]).style.textDecoration === "line-through red")
         {
             document.getElementById(ids[i]).style.textDecoration = "none";
         }
+    }
+}
+
+
+function updateCount(){
+    let strike = [0,0,0,0,0,0];  
+    let strikeBin = "";
+    for (let i = 0; i < ids.length; i++){
+        if (document.getElementById(ids[i]).style.textDecoration === "line-through red"){
+            strike[i] = 1;
+        }
+        strikeBin = strikeBin + strike[i].toString();
+    }
+    localStorage.setItem("strike",strikeBin);
+}
+
+if (localStorage.getItem("strike") === null)
+{
+    localStorage.setItem("strike","000000");
+}
+let strikeBin = localStorage.getItem("strike");
+
+for (let i = 0; i < strikeBin.length; i++){    
+    if (strikeBin[i] === "1"){
+        document.getElementById(ids[i]).style.textDecoration = "line-through red";
+    }
+    else{
+        document.getElementById(ids[i]).style.textDecoration = "none";
     }
 }
